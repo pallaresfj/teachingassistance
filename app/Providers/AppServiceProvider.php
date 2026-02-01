603c0ce9c0074c062fcdd75c4f3c589a2c4605d7
pallaresfj <pallaresfj@yahoo.es>
2026-01-31 22:10:04 -0500

    Agregar clase LogoutResponse para manejar la respuesta de cierre de sesión

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Filament\Auth\LogoutResponse as FilamentLogoutResponse;
+use Filament\Auth\Http\Responses\Contracts\LogoutResponse as LogoutResponseContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(LogoutResponseContract::class, FilamentLogoutResponse::class);
     }
 
     /**
