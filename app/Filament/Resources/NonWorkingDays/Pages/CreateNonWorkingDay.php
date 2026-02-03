462c3a31487cb47eb6044d14c768ec61f1fd07ad
pallaresfj <pallaresfj@yahoo.es>
2026-02-02 21:49:12 -0500

    fix: Redirigir a lista después de crear NonWorkingDay

--- app/Filament/Resources/NonWorkingDays/Pages/CreateNonWorkingDay.php
+++ app/Filament/Resources/NonWorkingDays/Pages/CreateNonWorkingDay.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateNonWorkingDay extends CreateRecord
 {
     protected static string $resource = NonWorkingDayResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
