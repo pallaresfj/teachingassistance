335d647e928a40c233505d447eeea2d51e1cd3ce
pallaresfj <pallaresfj@yahoo.es>
2026-02-02 21:43:25 -0500

    fix: Actualizar vista Blade de CreateVacationRange para Filament 5

--- resources/views/filament/resources/non-working-days/pages/create-vacation-range.blade.php
+++ resources/views/filament/resources/non-working-days/pages/create-vacation-range.blade.php
@@ -1,9 +1,19 @@
-<x-filament-panels::page>
-    <x-filament-panels::form wire:submit="create">
+<x-filament::page>
+    <form wire:submit="create" class="space-y-6">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-        />
-    </x-filament-panels::form>
-</x-filament-panels::page>
+        <div class="fi-form-actions">
+            <x-filament::button type="submit">
+                Crear Vacaciones
+            </x-filament::button>
+
+            <x-filament::button
+                color="gray"
+                tag="a"
+                :href="$this->getResource()::getUrl('index')"
+            >
+                Cancelar
+            </x-filament::button>
+        </div>
+    </form>
+</x-filament::page>
